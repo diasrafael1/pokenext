2bcafe6e08ba0600f6f1b1430a20ae4a2ad15222
Rafael Dias <rafaelldiias100@gmail.com>
2022-11-07 20:14:54 -0300

    feat: página da home parcialmente feita com a requisição da API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,48 @@
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <h1>PokéNext</h1>;
+interface Pokemon {
+  id?: number;
+  name: string;
+  url: string;
+}
+interface IDataPokemon {
+  results: Pokemon[];
+}
+
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = "https://pokeapi.co/api/v2/pokemon";
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data: IDataPokemon = await res.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }: { pokemons: Pokemon[] }) {
+  return (
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poké<span>Next</span>
+        </h1>
+        <Image
+          src="/images/pokeball.png"
+          width="50"
+          height="50"
+          alt="PokéNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}></div>
+    </>
+  );
 }
